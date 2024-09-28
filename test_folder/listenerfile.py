8aefaf64e84a47354b0d5afd5561be813011d2b9
Shakeel Nawaz <75359115+Shakeel-Nawaz@users.noreply.github.com>
2024-09-28 15:20:44 +0530

    Add files via upload

---
+++ test_folder/listenerfile.py
@@ -0,0 +1,13 @@
+ROBOT_LISTENER_API_VERSION = 3
+
+def start_suite(name, attributes):
+    print(f"Starting suite: {name}")
+
+def end_suite(name, attributes):
+    print(f"Ending suite: {name}")
+
+def start_test(name, attributes):
+    print(f"Starting test: {name}")
+
+def end_test(name, attributes):
+    print(f"Ending test: {name}")
